0fe8fac6e2bb7750bfab4c508bd2d3fe721c21c1
Alex Berthon <Alexjberthon@hotmail.com>
2019-05-25 21:46:36 -0700

    RenderImage
    RenderImage in frame

--- Game.java
+++ Game.java
@@ -3,20 +3,27 @@ package AdventureGame;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.awt.Graphics;
 import java.lang.Runnable;
 import java.lang.Thread;
 import javax.swing.JFrame;
+import javax.imageio.ImageIO;
 
 public class Game extends JFrame implements Runnable {
 	private Canvas canvas = new Canvas();
 	private RenderHandler renderer;
+	private BufferedImage testImage;
 	
 	public Game(){
 		//Program shutdown on exit
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//set position and size of the frame
+		setBounds(0, 0, 1000, 800);
+		
+		//centralize frame 
 		setLocationRelativeTo(null);
 		
 		//Add graphics component
@@ -29,16 +36,35 @@ public class Game extends JFrame implements Runnable {
 		canvas.createBufferStrategy(3);
 		
 		renderer = new RenderHandler(getWidth(), getHeight());
+		
+		testImage = loadImage("dark-blue.jpg"); //replace with image file. Might need to import into eclipse first
 	}
 	
 	public void update(){
 		
+	}
+	
+	private BufferedImage loadImage(String path){
+		try{
+			BufferedImage loadedImage = ImageIO.read(Game.class.getResource(path));
+			BufferedImage formattedImage = new BufferedImage(loadedImage.getWidth(), loadedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+			formattedImage.getGraphics().drawImage(loadedImage, 0, 0, null);
+			
+			return loadedImage; 
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public void render(){
 		BufferStrategy bufferStrategy = canvas.getBufferStrategy();
 		Graphics graphics = bufferStrategy.getDrawGraphics();
 		super.paint(graphics);
+		
+		renderer.renderImage(testImage, 0, 0);
+		renderer.render(graphics);
 		
 		graphics.dispose();
 		bufferStrategy.show();
--- RenderHandler.java
+++ RenderHandler.java
@@ -16,11 +16,23 @@ public class RenderHandler {
 		pixels = ((DataBufferInt) view.getRaster().getDataBuffer()).getData();
 	}
 	
+	//render array of pixels to the screen
 	public void render(Graphics graphics){
 		for(int i=0; i<pixels.length; i++){
 			pixels[i] = (int)(Math.random()*0xFFFFFF);
 		}
 		
 		graphics.drawImage(view,  0, 0, view.getWidth(), view.getHeight(), null);
+	}
+	
+	//Render image to array of pixels
+	public void renderImage(BufferedImage image, int xPosition, int yPosition){
+		int[] imagePixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+		for(int y = 0; y < image.getHeight(); y++){
+			for(int x = 0; x < image.getWidth(); x++){
+				pixels[(x + xPosition) + (y + yPosition) * view.getWidth()] = imagePixels[x + y * image.getWidth()];
+			}
+		}
 	}
 }
